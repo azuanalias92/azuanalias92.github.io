4cf6d99e8d457964632d735d1a716f6a9bb0a181
Azuan Alias <azuan@fulkrum.net>
2023-06-15 03:19:36 +0800

    First Commit

---
+++ constants/data.js
@@ -0,0 +1,98 @@
+const userData = {
+  githubUsername: "azuanalias92",
+  name: "Azuan Alias",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/father.jpg",
+  email: "azuanalias92@gmail.com",
+  phone: "+6019-4503378",
+  address: "Alor Setar, Kedah, Malaysia.",
+  projects: [
+    {
+      title: "Checkpoint Spot",
+      link: "https://checkpointspot.asia",
+      imgUrl: "/cps.png",
+    },
+    {
+      title: "USM Expert",
+      link: "http://ec2-18-140-212-199.ap-southeast-1.compute.amazonaws.com:8080/",
+      imgUrl: "/usm.png",
+    },
+    {
+      title: "TodoNext",
+      link: "https://todonext-alpha.vercel.app/",
+      imgUrl: "/todonext.png",
+    },
+    {
+      title: "ModResConverter",
+      link: "https://github.com/azuanalias92/ModResConverter/tree/v1.1.8",
+      imgUrl: "/modrest.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves building products and web applications that impact millions of lives",
+    description: [
+      `I've been developing full-stack application since I was 21 years old. I didn't know what full-stack meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
+      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+      `But now, I'm a legend. I've been coding in React, Angular, Vue, Svelte, Node.js, Express, Fastify, MongoDB, MySQL, Bootstrap, Tailwind, C, C++, C#, Rust, Haskell, Perl, PHP, Assembly, Logo and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
+    ],
+    currentProject: "Checkpoint Spot",
+    currentProjectUrl: "https://checkpointspot.asia",
+  },
+  experience: [
+    {
+      title: "Senior Software Developer",
+      company: "Checkpoint Spot",
+      year: "Current",
+      companyLink: "https://checkpointspot.asia",
+      desc: "Malaysia Running & Cycling Events Registration",
+    },
+    {
+      title: "Senior Software Developer",
+      company: "Fulkrum Media Interactive",
+      year: "2022",
+      companyLink: "https://fulkrum.net",
+      desc: "Tadaa! I'm in a Software Development house, making meanings through software and technology.",
+    },
+    {
+      title: "Analysts",
+      company: "Prestige Kinetic",
+      year: "2015",
+      companyLink: "http://www.prestigekinetic.com/",
+      desc: "Small stuff that matter. But work as contactor is not fun at all",
+    },
+   
+    {
+      title: "Graduation",
+      company: "Uninversiti Sains Malaysia (Bachelor of Computer Science)",
+      year: "2015",
+      companyLink: "https://cs.usm.my/",
+      desc: "Major in Computer Science with a CGPA of 3.1. Nobody asks this but it's okay.",
+    },
+    {
+      title: "Internship",
+      company: "Altera Corporation",
+      year: "2014",
+      companyLink: "https://altera.com",
+      desc: "I hate office jobs",
+    },
+    {
+      title: "High School",
+      company: "Maktab Rendah Sains Mara Langkawi",
+      year: "2009",
+      companyLink: "http://langkawi.mrsm.edu.my/",
+      desc: "4A+ 4A- 1B+. 1 Subject let me down.I guess we don't have any CHEMISTRY",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1nB304GjiI9Sxp7xpjUYM37TSXZ7CbGSd/view?usp=sharing",
+  socialLinks: {
+    twitter: "https://twitter.com/azuan_tra",
+    linkedin: "https://my.linkedin.com/in/azuanalias",
+    github: "https://github.com/azuanalias92",
+    peerlist: "https://peerlist.io/azuanalias",
+    instagram: "https://www.instagram.com/azuan_alias"
+  },
+};
+
+export default userData;
