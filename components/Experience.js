4c5a3b32477c38045de51f6a297a5b6067673469
Azuan Alias <azuanalias92@gmail.com>
2025-08-10 17:10:39 +0800

    Refactor : improve css

--- components/Experience.js
+++ components/Experience.js
@@ -7,8 +7,8 @@ export default function Experience() {
       <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
         <h1 className=" text-5xl md:text-9xl font-bold py-20 text-center md:text-left">Experience</h1>
       </div>
-      <div className="bg-[#F1F1F1] dark:bg-gray-900 -mt-4">
-        <div className="grid grid-cols-1 dark:bg-gray-900 max-w-xl mx-auto pt-20">
+      <div className="bg-[#F1F1F1] dark:bg-gray-900 -mt-10">
+        <div className="grid grid-cols-1 dark:bg-gray-900 max-w-xl mx-auto pt-36">
           {/* Experience card */}
           {userData.experience.map((exp, idx) => (
             <>
